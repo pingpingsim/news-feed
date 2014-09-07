6f5ec4d65dcda4821a7b0e2991b82596e88ba603
Sim <pingping.sim83@gmail.com>
2014-09-07 18:29:38 +0800

    add articlelistingarrayadapter class.

---
+++ app/src/main/java/simpp/newsfeed/android/data/ArticleListingArrayAdapter.java
@@ -0,0 +1,53 @@
+package simpp.newsfeed.android.data;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import simpp.newsfeed.android.R;
+import simpp.newsfeed.android.model.Article;
+import simpp.newsfeed.android.model.SubCategory;
+
+/**
+ * This class assigns category_fragment's status information to the list view's row
+ * Created by user on 8/31/2014.
+ */
+public class ArticleListingArrayAdapter extends ArrayAdapter<Article> {
+    private Context context;
+    private List<Article> data;
+
+    public ArticleListingArrayAdapter(Context context, int layoutResourceId, List<Article> data) {
+        super(context, layoutResourceId, data);
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public int getCount(){
+        return data.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        if (view == null) {
+            final LayoutInflater layoutInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.article_row, null);
+        }
+        final Article article = data.get(position);
+        if (article != null)
+        {
+            TextView display = (TextView) view
+                    .findViewById(R.id.article_display_name);
+            display.setText(article.getTitle());
+        }
+        return view;
+    }
+}
